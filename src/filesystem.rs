1369898a0142b68c4be7a69bb417e1076127ccf5
Andre Popovitch <andre@popovit.ch>
2023-08-21 21:32:00 -0500

    switch to pub(crate)

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -6,13 +6,13 @@ use web_sys::{
 };
 
 #[derive(Debug)]
-pub struct DirectoryHandle(FileSystemDirectoryHandle);
+pub(crate) struct DirectoryHandle(FileSystemDirectoryHandle);
 
 #[derive(Debug)]
-pub struct FileHandle(FileSystemFileHandle);
+pub(crate) struct FileHandle(FileSystemFileHandle);
 
 #[derive(Debug)]
-pub struct WritableFileStream(FileSystemWritableFileStream);
+pub(crate) struct WritableFileStream(FileSystemWritableFileStream);
 
 impl From<FileSystemDirectoryHandle> for DirectoryHandle {
     fn from(handle: FileSystemDirectoryHandle) -> Self {
@@ -33,7 +33,7 @@ impl From<FileSystemWritableFileStream> for WritableFileStream {
 }
 
 impl DirectoryHandle {
-    pub async fn get_file_handle_with_options(
+    pub(crate) async fn get_file_handle_with_options(
         &self,
         name: &str,
         options: &FileSystemGetFileOptions,
@@ -46,7 +46,7 @@ impl DirectoryHandle {
 }
 
 impl FileHandle {
-    pub async fn create_writable_with_options(
+    pub(crate) async fn create_writable_with_options(
         &self,
         options: &FileSystemCreateWritableOptions,
     ) -> Result<WritableFileStream, JsValue> {
@@ -56,29 +56,29 @@ impl FileHandle {
         Ok(WritableFileStream(file_system_writable_file_stream))
     }
 
-    pub async fn get_file(&self) -> Result<Blob, JsValue> {
+    pub(crate) async fn get_file(&self) -> Result<Blob, JsValue> {
         let file: Blob = JsFuture::from(self.0.get_file()).await.unwrap().into();
         Ok(file)
     }
 
-    pub async fn get_size(&self) -> Result<f64, JsValue> {
+    pub(crate) async fn get_size(&self) -> Result<f64, JsValue> {
         let file: Blob = JsFuture::from(self.0.get_file()).await.unwrap().into();
         Ok(file.size())
     }
 }
 
 impl WritableFileStream {
-    pub async fn write_with_u8_array(&self, data: &mut [u8]) -> Result<(), JsValue> {
+    pub(crate) async fn write_with_u8_array(&self, data: &mut [u8]) -> Result<(), JsValue> {
         JsFuture::from(self.0.write_with_u8_array(data)?).await?;
         Ok(())
     }
 
-    pub async fn close(&self) -> Result<(), JsValue> {
+    pub(crate) async fn close(&self) -> Result<(), JsValue> {
         JsFuture::from(self.0.close()).await?;
         Ok(())
     }
 
-    pub async fn seek_with_f64(&self, offset: f64) -> Result<(), JsValue> {
+    pub(crate) async fn seek_with_f64(&self, offset: f64) -> Result<(), JsValue> {
         JsFuture::from(self.0.seek_with_f64(offset).unwrap()).await?;
         Ok(())
     }
