425b7b68d7e875271fc021aa9a10674b5d1348bd
Sam Hall <39913933+shmall27@users.noreply.github.com>
2023-08-21 19:16:04 -0700

    Append embedding to end of bin (#4)

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,8 +1,8 @@
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-    FileSystemDirectoryHandle, FileSystemFileHandle, FileSystemGetFileOptions,
-    FileSystemWritableFileStream,
+    Blob, FileSystemCreateWritableOptions, FileSystemDirectoryHandle, FileSystemFileHandle,
+    FileSystemGetFileOptions, FileSystemWritableFileStream,
 };
 
 #[derive(Debug)]
@@ -46,11 +46,24 @@ impl DirectoryHandle {
 }
 
 impl FileHandle {
-    pub async fn create_writable(&self) -> Result<WritableFileStream, JsValue> {
+    pub async fn create_writable_with_options(
+        &self,
+        options: &FileSystemCreateWritableOptions,
+    ) -> Result<WritableFileStream, JsValue> {
         let file_system_writable_file_stream = FileSystemWritableFileStream::unchecked_from_js(
-            JsFuture::from(self.0.create_writable()).await?,
+            JsFuture::from(self.0.create_writable_with_options(options)).await?,
         );
         Ok(WritableFileStream(file_system_writable_file_stream))
+    }
+
+    pub async fn get_file(&self) -> Result<Blob, JsValue> {
+        let file: Blob = JsFuture::from(self.0.get_file()).await.unwrap().into();
+        Ok(file)
+    }
+
+    pub async fn get_size(&self) -> Result<f64, JsValue> {
+        let file: Blob = JsFuture::from(self.0.get_file()).await.unwrap().into();
+        Ok(file.size())
     }
 }
 
@@ -62,6 +75,11 @@ impl WritableFileStream {
 
     pub async fn close(&self) -> Result<(), JsValue> {
         JsFuture::from(self.0.close()).await?;
+        Ok(())
+    }
+
+    pub async fn seek_with_f64(&self, offset: f64) -> Result<(), JsValue> {
+        JsFuture::from(self.0.seek_with_f64(offset).unwrap()).await?;
         Ok(())
     }
 }
