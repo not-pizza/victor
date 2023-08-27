27c4900ee9bc1c31867a3eefc1371a22c72ac435
Andre Popovitch <andre@popovit.ch>
2023-08-27 15:29:40 -0500

    add in-memory filesystem (haven't tested yet)

--- src/filesystem/memory.rs
+++ src/filesystem/memory.rs
@@ -1,29 +1,29 @@
 //! "in-memory" filesystem for use in tests or when persistence isn't necessary
 
-use std::{
-    cell::{RefCell, RefMut},
-    collections::HashMap,
-    sync::{RwLock, RwLockWriteGuard},
-};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use async_trait::async_trait;
 
 use crate::filesystem;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) enum DirectoryEntry {
-    Directory(RwLock<DirectoryHandle>),
-    File(RwLock<FileHandle>),
+    #[allow(dead_code)]
+    Directory(DirectoryHandle),
+    File(FileHandle),
 }
 
-#[derive(Debug)]
-pub(crate) struct DirectoryHandle(HashMap<String, RefCell<DirectoryEntry>>);
+#[derive(Debug, Clone)]
+pub(crate) struct DirectoryHandle(Rc<RefCell<HashMap<String, DirectoryEntry>>>);
 
-#[derive(Debug)]
-pub(crate) struct FileHandle(RefCell<Vec<u8>>);
+#[derive(Debug, Clone)]
+pub(crate) struct FileHandle(WritableFileStream);
 
-#[derive(Debug)]
-pub(crate) struct WritableFileStream<'a>(RefMut<'a, Vec<u8>>);
+#[derive(Debug, Clone)]
+pub(crate) struct WritableFileStream {
+    cursor_pos: usize,
+    stream: Rc<RefCell<Vec<u8>>>,
+}
 
 #[async_trait(?Send)]
 impl filesystem::DirectoryHandle for DirectoryHandle {
@@ -31,21 +31,107 @@ impl filesystem::DirectoryHandle for DirectoryHandle {
     type FileHandleT = FileHandle;
 
     async fn get_file_handle_with_options(
-        &self,
+        &mut self,
         name: &str,
         options: &filesystem::GetFileHandleOptions,
     ) -> Result<Self::FileHandleT, Self::Error> {
-        let directory = self.0;
-        let entry = directory.get(name);
-        
+        let mut directory = self.0.borrow_mut();
+        let entry = match directory.entry(name.to_string()) {
+            std::collections::hash_map::Entry::Occupied(entry) => entry.get().clone(),
+            std::collections::hash_map::Entry::Vacant(entry) => {
+                if options.create {
+                    let file_handle = FileHandle::new();
+                    entry.insert(DirectoryEntry::File(file_handle.clone()));
+                    DirectoryEntry::File(file_handle)
+                } else {
+                    return Err(format!("'{name}' does not exist"));
+                }
+            }
+        };
 
-        let file_system_file_handle = FileSystemFileHandle::from(
-            JsFuture::from(self.0.get_file_handle_with_options(
-                name,
-                FileSystemGetFileOptions::new().create(options.create),
-            ))
-            .await?,
-        );
-        Ok(FileHandle(file_system_file_handle))
+        match entry {
+            DirectoryEntry::Directory(_) => Err(format!("'{name}' is a directory")),
+            DirectoryEntry::File(file) => Ok(file),
+        }
+    }
+}
+
+#[async_trait(?Send)]
+impl filesystem::FileHandle for FileHandle {
+    type Error = String;
+    type WritableFileStreamT = WritableFileStream;
+
+    async fn create_writable_with_options(
+        &mut self,
+        options: &filesystem::CreateWritableOptions,
+    ) -> Result<Self::WritableFileStreamT, Self::Error> {
+        if options.keep_existing_data {
+            Ok(WritableFileStream {
+                cursor_pos: 0,
+                ..self.0.clone()
+            })
+        } else {
+            self.0.stream.borrow_mut().clear();
+            Ok(WritableFileStream {
+                cursor_pos: 0,
+                ..self.0.clone()
+            })
+        }
+    }
+
+    async fn read(&self) -> Result<Vec<u8>, Self::Error> {
+        let stream: Rc<RefCell<Vec<u8>>> = self.0.stream.clone();
+        let data = stream.borrow().clone();
+        Ok(data)
+    }
+
+    async fn get_size(&self) -> Result<usize, Self::Error> {
+        Ok(self.0.len())
+    }
+}
+
+#[async_trait(?Send)]
+impl filesystem::WritableFileStream for WritableFileStream {
+    type Error = String;
+
+    async fn write_at_cursor_pos(&mut self, data: Vec<u8>) -> Result<(), Self::Error> {
+        if self.cursor_pos != self.stream.borrow().len() {
+            // tbh I don't know what happens if you try to write but your cursor isn't at the end
+            // and we never do that so who cares
+            todo!()
+        }
+        self.stream
+            .borrow_mut()
+            .extend(data.into_iter().collect::<Vec<u8>>());
+        Ok(())
+    }
+
+    async fn close(&mut self) -> Result<(), Self::Error> {
+        // no op
+        Ok(())
+    }
+
+    async fn seek(&mut self, offset: usize) -> Result<(), Self::Error> {
+        self.cursor_pos = offset;
+        Ok(())
+    }
+}
+
+impl FileHandle {
+    fn new() -> Self {
+        Self(WritableFileStream::new())
+    }
+}
+
+impl WritableFileStream {
+    fn new() -> Self {
+        Self {
+            cursor_pos: 0,
+            stream: Rc::new(RefCell::new(Vec::new())),
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.stream.borrow().len()
     }
 }
--- src/filesystem/mod.rs
+++ src/filesystem/mod.rs
@@ -1,4 +1,4 @@
-//mod memory;
+mod memory;
 pub mod web;
 
 use std::fmt::Debug;
